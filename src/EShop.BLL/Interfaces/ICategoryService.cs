643cf26dff212703fb0fb6422e40fd90ea6849e5
Andrey Sivy <sivyandrey2021@gmail.com>
2024-06-18 22:21:27 +0300

    Add service interfaces

---
+++ src/EShop.BLL/Interfaces/ICategoryService.cs
@@ -0,0 +1,21 @@
+﻿using EShop.BLL.DTO.Category;
+
+namespace EShop.BLL.Interfaces;
+
+public interface ICategoryService
+{
+    Task<ReadCategoryDto?> CreateCategoryAsync(
+        CreateCategoryDto categoryDto, CancellationToken cancellationToken = default);
+    
+    Task<IEnumerable<ReadCategoryDto>> GetCategoriesAsync(
+        CancellationToken cancellationToken = default);
+    
+    Task<ReadCategoryDto?> UpdateCategoryAsync(
+        Guid id, CreateCategoryDto categoryDto, CancellationToken cancellationToken = default);
+    
+    Task<bool> DeleteCategoryAsync(
+        Guid id, CancellationToken cancellationToken = default);
+    
+    Task<ReadCategoryDto?> GetCategoryByIdAsync(
+        Guid id, CancellationToken cancellationToken = default);
+}
