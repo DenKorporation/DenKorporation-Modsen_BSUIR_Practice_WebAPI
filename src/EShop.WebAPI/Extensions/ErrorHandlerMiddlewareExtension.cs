ebf48ac7aacd5b3bc94c42f6d0789252143cb266
Andrei Sivy <93795081+andrw04@users.noreply.github.com>
2024-06-20 16:35:31 +0300

    add global exception handler (#7)

---
+++ src/EShop.WebAPI/Extensions/ErrorHandlerMiddlewareExtension.cs
@@ -0,0 +1,11 @@
+﻿using EShop.WebAPI.Middleware;
+
+namespace EShop.WebAPI.Extensions;
+
+public static class ErrorHandlerMiddlewareExtension
+{
+    public static IApplicationBuilder UseErrorHandler(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<ErrorHandlerMiddleware>();
+    }
+}
---
+++ src/EShop.WebAPI/Middleware/ErrorHandlerMiddleware.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+using System.Text.Json;
+using EShop.WebAPI.Constants;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EShop.WebAPI.Middleware;
+
+public class ErrorHandlerMiddleware : IMiddleware
+{
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception exception)
+        {
+            var statusCode = HttpStatusCode.InternalServerError;
+
+            statusCode = exception switch
+            {
+                // TODO - add custom exception types
+                ValidationException e => HttpStatusCode.BadRequest,
+                _ => statusCode
+            };
+
+            context.Response.ContentType = ContentTypes.ApplicationJson;
+            context.Response.StatusCode = (int)statusCode;
+            
+            var errors = string.Empty;
+
+            if (exception is ValidationException validationException)
+            {
+                var errorMessages = validationException.Errors
+                    .Select(e => e.ErrorMessage)
+                    .ToArray();
+                errors = string.Join('\n', errorMessages);
+            }
+
+            var errorResponse = new
+            {
+                Timestamp = DateTime.Now,
+                StatusCode = statusCode,
+                Error = string.IsNullOrEmpty(errors) ? "Something went wrong." : errors
+            };
+
+            await context.Response.WriteAsync(JsonSerializer.Serialize(errorResponse));
+        }
+    }
+}
