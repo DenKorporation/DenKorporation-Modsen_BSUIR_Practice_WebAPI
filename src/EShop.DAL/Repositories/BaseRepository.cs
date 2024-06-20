450a3e4bb8131a525c9db4ef878b4ddd27e8082e
iamnovik <92405752+iamnovik@users.noreply.github.com>
2024-06-20 15:36:08 +0300

    Repositories implementations (#6)
    * Repositories implementations
    
    * UnitOfWork added
    
    * BaseRepository added
    
    * Names were changed. Cancellation tokens, AsNoTracking were addeed

---
+++ src/EShop.DAL/Repositories/BaseRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EShop.DAL.Interfaces;
+
+public abstract class BaseRepository<TEntity, TKey> : IBaseRepository<TEntity, TKey> where TEntity : class
+{
+    protected readonly EShopContext _context;
+    protected readonly DbSet<TEntity> _dbSet;
+
+    public BaseRepository(EShopContext context)
+    {
+        _context = context;
+        _dbSet = _context.Set<TEntity>();
+    }
+
+    public virtual async Task<TEntity> AddAsync(TEntity entity, CancellationToken cancellationToken = default)
+    {
+        await _dbSet.AddAsync(entity, cancellationToken);
+        return entity;
+    }
+
+    public virtual async Task<TEntity> GetByIdAsync(TKey id, CancellationToken cancellationToken = default)
+    {
+        return await _dbSet.AsNoTracking().SingleOrDefaultAsync(e=> e.Id == id, cancellationToken);
+    }
+
+    public virtual Task Update(TEntity entity)
+    {
+        _dbSet.Update(entity);
+        return Task.CompletedTask;
+    }
+
+    public virtual async Task DeleteAsync(TKey id, CancellationToken cancellationToken = default)
+    {
+        var entity = await _dbSet.FindAsync(id, cancellationToken);
+        _dbSet.Remove(entity);
+    }
+
+    public virtual async Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken = default)
+    {
+        return await _dbSet.AsNoTracking().ToListAsync(cancellationToken);
+    }
+
+    public virtual async Task<IEnumerable<TEntity>> FilterAsync(Func<TEntity, bool> predicate, CancellationToken cancellationToken = default)
+    {
+        return await Task.FromResult(_dbSet.AsNoTracking().Where(predicate).ToListAsync(cancellationToken));
+    }
+}
---
+++ src/EShop.DAL/Repositories/OrderItemRepository.cs
@@ -0,0 +1,14 @@
+﻿using EShop.DAL.Context;
+using EShop.DAL.Entities;
+using EShop.DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace EShop.DAL.Repositories;
+
+public class OrderItemRepository : BaseRepository<OrderItem, int>,IOrderItemRepository
+{
+    public OrderItemRepository(EShopContext context) : base(context)
+    {
+    }
+    
+}
