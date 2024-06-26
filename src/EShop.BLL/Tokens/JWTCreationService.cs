12b185328becdfab99021c4819eebe04aa29d5d7
Егор <e.timoshek@mail.ru>
2024-06-26 23:33:03 +0300

    Генерация jwt токенов

---
+++ src/EShop.BLL/Tokens/JWTCreationService.cs
@@ -0,0 +1,38 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+
+namespace EShop.BLL.Tokens
+{
+    public class JwtCreationService
+    {
+        private readonly string _issuer;
+        private readonly string _audience;
+        private readonly SymmetricSecurityKey _signingKey;
+        private readonly int _expirationMinutes;
+
+        public JwtCreationService(string issuer, string audience, string signingKey, int expirationMinutes)
+        {
+            _issuer = issuer;
+            _audience = audience;
+            _signingKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(signingKey));
+            _expirationMinutes = expirationMinutes;
+        }
+
+        public string GenerateToken(ClaimsIdentity claimsIdentity)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = claimsIdentity,
+                Issuer = _issuer,
+                Audience = _audience,
+                Expires = DateTime.UtcNow.AddMinutes(_expirationMinutes),
+                SigningCredentials = new SigningCredentials(_signingKey, SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
